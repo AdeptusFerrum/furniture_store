fe9648c86fdb03e686b9048d4dac0a89606b68d2
AdeptusFerrum <vitalik.kleymenov.91@mail.ru>
2025-07-13 18:41:07 +0300

    tables. first version

---
+++ tables.sql
@@ -0,0 +1,266 @@
+create table if not exists transit_status(
+    transit_status_id serial not null constraint pk_transit_status_id,
+    transit_status_name varchar(36) not null
+);
+
+create table if not exists courier(
+    courier_id serial not null constraint pk_courier_id primary key,
+    courier_surname varchar(36) not null,
+    courier_name varchar(36) not null,
+    courier_second_name varchar(36) null,
+    courier_telephone varchar(16) not null  
+);
+
+create table if not exists Model_Automobile(
+    Model_Automobile_id serial not null constraint pk_model_automobile_id,
+    Model_automobile_name varchar(50) not null 
+);
+create table if not exists color_automobile(
+    color_automobile_id serial not null constraint pk_color_automobile_id,
+    color_automobile_name varchar(36) not null
+);
+
+create table if not exists mark_automobile(
+    mark_automobile_id serial not null constraint mark_automobile primary key,
+    mark_automobile_name varchar(36) not null
+);
+
+create table if not exists Product_Status (
+    Product_Status_id serial not null constraint PK_Product_Status_ID primary key,
+    Product_Status_Name varchar(50) not null
+);
+
+create table if not exists Users(
+    users_id serial not null  constraint pk_users primary key,
+    users_login varchar(36) not null  not null,
+    users_password varchar(255) not null  
+);
+
+create table if not exists Storages(
+    Storages_ID serial not null constraint PK_Storages_ID primary key,
+    Storages_Number varchar(6) not null,
+    Storages_Address text not null
+);
+
+create table if not exists Material(
+    Material_ID serial not null constraint PK_Material_ID primary key,
+    Material_Name varchar(50) not null
+);
+
+create table if not exists Order_Status(
+    Order_Status_ID serial not null constraint PK_Order_Status_ID primary key,
+    Order_Status_Name varchar(50) not null
+);
+
+create table if not exists Employee_Organization(
+    Employee_Organization_ID serial not null constraint PK_Employee_Organization_Id primary key,
+    Employee_Organization_Surname varchar(50) not null,
+    Employee_Organization_Name varchar(50) not null,
+    Employee_Organization_Second_Name varchar(50) null
+);
+
+create table if not exists Legal_Form(
+    Legal_Form_ID serial not null constraint PK_Legal_Form_Id primary key,
+    Legal_Form_Name varchar(5) not null
+);
+
+create table if not exists Type(
+    Type_Id serial not null constraint PK_Type_Id primary key,
+    Type_Name varchar(20) not null
+);
+
+create table if not exists Country(
+    Country_Id serial not null constraint pk_country_id primary key,
+    Country_Name varchar(50) not null
+);
+
+create table if not exists Mark(
+    Mark_Id serial not null constraint pk_mark_id primary key,
+    Country_Id int not null references Country(Country_ID),
+    Mark_Name varchar(30) not null
+);
+
+create table if not exists Model(
+    Model_ID serial not null constraint pk_model_id primary key,
+    Mark_ID int not null references Mark(Mark_Id),
+    Model_Name varchar(50) not null
+)
+
+create table if not exists Product(
+    Product_ID serial not null constraint PK_Product_ID primary key,
+    Model_ID int not null references Model(Model_Id),
+    Type_ID int not null references Type(Type_Id),
+    Product_Article varchar(11) not null,
+    Product_Price decimal(12,2) not null,
+    Product_Length int not null,
+    Product_Width int not null,
+    Product_Height int not null,
+    Product_Description text null,
+    Product_Photo varchar(100) null
+);
+
+create table if not exists Product_Material(
+    Product_Material_ID serial not null constraint pk_product_material_key primary key,
+    Product_Id int references Product(Product_ID),
+    Material_Id int references Material(Material_Id)
+);
+
+create table if not exists Product_Avaliable(
+    Proudct_Avaliable_ID serial not null constraint product_avaliable_id primary key,
+    Product_ID int references Product(Product_ID),
+    Product_Status_ID int references Product_Status(Product_Status_Id),
+    Storages_ID int references Storages(Storages_ID),
+    Product_Avaliable int not null
+);
+
+create table if not exists Manufacturer (
+    Manufacturer_ID serial not null constraint pk_manufacturer_id primary key,
+    Legal_Form_ID int not null references Legal_Form(Legal_Form_ID),
+    Manufacturer_Name varchar(50) not null,
+    Manufacturer_Address text not null,
+    Manufacturer_Legal_Address text not null,
+    Manufacturer_TIN varchar(10) not null,
+    Manufacturer_OKPO varchar(10) not null,
+    Manufacturer_BIC varchar(9) not null
+);
+
+create table if not exists Estimate(
+    Estimate_Id serial not null constraint pk_estimate_id primary key,
+    Manufacturer_id int not null references Manufacturer(Manufacturer_ID)
+    Employee_Organization_Id int not null references Employee_Organization(Employee_Organization_ID),
+    Estimate_Number varchar(14) not null,
+    Estimate_Date date not null
+);
+
+create table if not exists Estimate_Content(
+    Estimate_Content_ID serial not null constraint pk_estimate_content_id primary key,
+    Estimate_ID int not null references Estimate(EStimate_id),
+    Product_ID int not null references Product(Product_ID),
+    Estimate_Content_Count int not null 
+);
+
+create table if not exists Defective_Elements(
+    Defective_Elements serial not null constraint pk_defective_elements_id primary key,
+    Estimate_Content_Id int not null references Estimate_Content(Estimate_Content_ID),
+    Defective_Elements_name varchar(50) not null
+);
+
+create table if not exists Delivery(
+    Delivery_ID serial not null constraint pk_delivery_id primary key,
+    Estimate_Id int not null references Estimate(Estimate_Id),
+    Delivery_date date not null,
+    Delivery_time time not null
+);
+
+create table if not exists Employee_Post(
+    Employee_Post_id serial not null constraint pk_delivery_id primary key,
+    Employee_Post_name varchar(50) not null
+);
+
+create table if not exists Employee(
+    Employee_Post_ID serial not null constraint pk_employee_post_id primary key,
+    Employee_Surname varchar(50) not null,
+    Employee_Name varchar(50) not null,
+    Employee_Second_Name varchar(50) null,
+    Employee_Telephone varchar(16) not null
+);
+
+create table if not exists Employee_Auth(
+    Employee_Auth_Id serial not null constraint pk_employee_auth_id primary key,
+    Employee_Id int unique not null references Employee(Employee_ID),
+    Users_id int unique not null  references Users(users_id)
+);
+
+create table if not exists customers_org(
+    customers_org_id serial not null constraint pk_organizations_id primary key,
+    legal_form_id int not null references Legal_Form(legal_form_id),
+    customers_org_full_name text not null,
+    customers_org_abbreviated_name varchar(50) not null,
+    customers_org_address text not null
+);
+
+create table if not exists orderr(
+    order_id serial not null constraint pk_order_id primary key,
+    employee_id int not null references employee(employee_id),
+    order_status_id int not null references order_status(order_status_id),
+    customers_org_id int not null references customers_org(customers_org_id),
+    order_number varchar(16) not null,
+    order_date date not null,
+    order_time time not null,
+    order_price decimal(12,2) not null
+);
+
+create table if not exists agent_post(
+    agent_post_id serial not null constaint pk_agent_post_id primary key,
+    agent_post_name varchar(50) not null
+);
+
+create table if not exists agent_auth (
+    agent_auth_id serial not null constraint pk_agent_auth_id,
+    users_id int not null references users(users_id),
+    agent_id int not null references agent(agent_id)
+);
+
+create table if not exists agent(
+    agent_id serial not null constraint pk_agent_id primary key,
+    agent_post_id int not null references agent_post(agent_post_id),
+    customers_org_id int not null references customers_org(customers_org_id),
+    agent_surname varchar(50) not null,
+    agent_name varchar(50) not null,
+    agent_second_name varchar(50) null,
+    agent_passport_series varchar(4) not null,
+    agent_passport_number varchar(6) not null,
+    agent_telephone varchar(16) not null,
+    agent_email text null
+);
+
+create table if not exists Order_Check(
+    Order_Check_id serial not null constraint pk_order_check_id primary key,
+    Order_Id int not null references orderr(order_id),
+    Order_Check_Number varchar(14) not null constraint unique UQ_Order_Check,
+    Order_Check_Price decimal(12,2) not null,
+    Order_Check_date date not null,
+    Order_Check_time time not null
+);
+
+create table if not exists Order_Content(
+    Order_content_id serial not null constraint pk_order_content_id primary key,
+    order_id int not null references orderr(order_id),
+    product_id int not null references product(product_id),
+    order_content_count int not null,
+    order_content_price decimal(12,2)
+);
+
+create table if not exists country_automobile(
+    country_automobile_id serial not null constraint pk_country_automobile_id primary key,
+    country_automobile_name varchar(3) not null
+);
+
+create table if not exists region_automobile(
+    region_automobile_id serial not null constraint pk_region_automobile_id primary key,
+    region_automobile_number int varchar(6)
+);
+
+create table if not exists automobile(
+    automobile_id serial not null constraint pk_automobile_id primary key,
+    mark_automobile_id int not null references mark_automobile(mark_automobile_id),
+    model_automobile_id int not null references model_automobile(model_automobile_id),
+    color_automobile_id int not null references color_automobile(color_automobile_id),
+    country_automobile int not null references country_automobile(country_automobile_id),
+    region_automobile int not null references region_automobile(region_automobile_id),
+    automobile_number varchar(6) not null
+);
+
+create table if not exists transit(
+    transit_id serial not null constraint pk_transit_id primary key,
+    transit_status_id int not null references transit_status(transit_status_id),
+    order_id int not null references orderr(order_id),
+    automobile_id int not null references automobile(automobile_id),
+    courier_id int not null references courier(courier_id),
+    transit_number varchar(16) not null,
+    transit_date date not null,
+    transit_time time not null,
+    transit_send date not null,
+    transit_arrive date not null,
+    transit_arrive_time time not null
+);
